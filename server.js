73ba3d51acdf08bac8afe7ea19f46d21a66cc49c
Jared Sutch <sutch002@gmail.com>
2021-06-21 21:08:16 -0500

    updated server js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/nameless-harbor-04256", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useNewUrlParser: true,
